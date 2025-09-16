2d7e3b0ca5ffb1a5af267ca0d53ebf43ff2a9893
Jhon-036 <jhondavid19p@gmail.com>
2025-09-15 21:52:51 -0500

    configuracion agregado al devolver resultados de la base de datos

--- src/models/productsModel.js
+++ src/models/productsModel.js
@@ -23,4 +23,16 @@ const productSchema = new mongoose.Schema({
   timestamps: true // agrega fechaCreacion y fechaUltimaActualizacion
 })
 
+productSchema.set('toJSON', {
+  transform: (doc, ret) => {
+    ret.id = ret._id
+    delete ret._id
+    delete ret.__v
+    delete ret.imagePublicId
+    delete ret.createdAt
+    delete ret.updatedAt
+    return ret
+  }
+})
+
 export default mongoose.model('Product', productSchema)
