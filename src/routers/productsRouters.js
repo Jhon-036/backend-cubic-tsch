055d69aabb2358aed9b54585c025399f1d2781ff
Jhon-036 <jhondavid19p@gmail.com>
2025-09-15 21:58:09 -0500

    middleware agregado a ruta get.products

--- src/routers/productsRouters.js
+++ src/routers/productsRouters.js
@@ -1,10 +1,11 @@
 import express from 'express'
 import {getProducts,getProduct,createProduct,updateProduct,deleteProduct} from '../controllers/productsControllers.js'
 import upload from '../config/upload.js'
+import { apiKey } from '../middleware/apiKey.js'
 
 const router = express.Router()
 
-router.get('/products', getProducts)
+router.get('/products', apiKey, getProducts)
 router.get('/products/:id', getProduct)
 router.post('/products', upload.single('image'), createProduct)
 router.put('/products/:id', upload.single('image'), updateProduct)
