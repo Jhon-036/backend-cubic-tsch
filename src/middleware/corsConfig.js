252cd3cea846e580a427c0297b7be57211aa80de
Jhon-036 <jhondavid19p@gmail.com>
2025-09-20 07:47:37 -0500

    Como dominio se agrega el localhost

--- src/middleware/corsConfig.js
+++ src/middleware/corsConfig.js
@@ -3,7 +3,7 @@
 import cors from 'cors'
 
 export const corsConfig = cors({
-  origin: ['https://tschperu.com'], // dominio
+  origin: ['https://tschperu.com', 'http://localhost:5173'], // dominio
   methods: ['GET'], // metodo permitido
   allowedHeaders: ["Content-Type","Authorization"], // Permite contenido en el body (application/json, multipart/form-data, etc.)
   credentials: false // Permite cookies
